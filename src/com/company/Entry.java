45b7b6ac15869154812b9682973c713548d3a7af
Jeffkent01coder <geofreyerastus956@gmail.com>
2021-08-30 09:55:14 +0300

    Initial commit

---
+++ src/com/company/Entry.java
@@ -0,0 +1,19 @@
+package com.company;
+
+import javax.swing.*;
+
+public class Entry {
+    private JPanel new_Data;
+    private JPanel labels;
+    private JPanel fields;
+    private JPanel buttons;
+    private JLabel label1;
+    private JLabel label2;
+    private JLabel label3;
+    private JLabel label4;
+    private JTextField user;
+    private JTextField f_name;
+    private JTextField P_number;
+    private JTextField email;
+    private JButton add_data;
+}
